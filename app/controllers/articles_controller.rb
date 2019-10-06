233471e99d85bb9f12170d51a7a3a47dde3b372c
takeda-shin <kgcfy958@yahoo.co.jp>
2019-10-06 19:05:18 +0900

    edit new post action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(create_params)
+    redirect_to action: :index
+  end
+
+  def create_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:new, :create]
+
   root to: "articles#index"
-  
-  get  "articles/new" => "articles#new" #記事投稿画面
 end
