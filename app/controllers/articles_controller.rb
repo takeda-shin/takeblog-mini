2dfa9d9ff9e639646b14eb600d38b5095da981e2
takeda-shin <kgcfy958@yahoo.co.jp>
2019-10-08 17:43:33 +0900

    add login function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     @articles = Article.all.order("id DESC").page(params[:page]).per(5)
   end
@@ -9,13 +11,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    Article.create(article_params)
+    Article.create(title: article_params[title], text: article_params[text], user_id: current_user.id)
     redirect_to root_path
   end
   
   private
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
